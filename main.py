638827667c3dc1a18d3bd145b9d93ce2a25227bd
ddMax <logumb@gmail.com>
2017-04-17 23:45:39 +0800

    [Modify] refine event hooks

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import random
+import string
+
+import gridfs
 
 from eve import Eve
 from flask import jsonify, request, redirect, flash
@@ -6,14 +10,11 @@ from flask.helpers import make_response
 from werkzeug.utils import secure_filename
 
 from classifier import clf
-from hooks import add_token, gen_classification_result
 from settings import (UPLOAD_FOLDER, ALLOWED_EXTENSIONS,
                       CODE_CLASSIFY_SUCCESS, MESSAGE_CLASSIFY_SUCCESS,
                       HOST, PORT)
 
 app = Eve()
-app.on_insert_users += add_token
-app.on_insert_images += gen_classification_result
 app.config['SECRET_KEY'] = "7;RE:]@xL_lp^TybQjfsq&'5M,!h\x0c+/S"
 # app.config['DEBUG'] = True
 
@@ -21,6 +22,40 @@ app.config['SECRET_KEY'] = "7;RE:]@xL_lp^TybQjfsq&'5M,!h\x0c+/S"
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def add_token(users):
+    """
+    Add a token when POST (register) /users
+    """
+    pool = string.ascii_letters + string.digits
+    for user in users:
+        user["token"] = ''.join(random.sample(pool, 32))
+
+
+def gen_classification_result(documents):
+    """
+    Perform classification and write result when POST (upload) /images
+    """
+    for document in documents:
+        image_object = document.get('image')
+        fs = gridfs.GridFS(app.data.driver.db)
+        image_data = fs.get(image_object)
+        result = clf.classify_image(image_data.read())
+        document['result'] = result
+
+
+def post_image_upload_callback(request, payload):
+    """
+    Add extra information when successfully classify the image
+    """
+    print('test')
+
+
+# Subscribe event hooks
+app.on_insert_users += add_token
+app.on_insert_images += gen_classification_result
+app.on_post_post_images += post_image_upload_callback
 
 
 @app.route('/images-demo', methods=['GET', 'POST'])
